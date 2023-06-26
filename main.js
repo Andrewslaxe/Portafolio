40f4e1c0d3c09f9958efbfd54e145151f113b788
Kengan <jaimeperaza2001@gmail.com>
2023-06-25 23:35:16 -0500

    Desktop version in work section

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
+let flagHeaderShow = true
 let prevScrollPos = window.scrollY
 
-let flagHeaderShow = true
-
 window.onscroll = function () {
-  let currentScrollPos = window.scrollY
-
   if (flagHeaderShow) {
+    let currentScrollPos = window.scrollY
     if (prevScrollPos > currentScrollPos) {
       document.getElementById("header").classList.remove("hide")
     } else {
@@ -14,6 +12,7 @@ window.onscroll = function () {
     prevScrollPos = currentScrollPos
   }
 }
+
 
 const toggleNav = () => {
   document.getElementById("nav").classList.toggle("hide")
@@ -36,18 +35,26 @@ const scrollToSection = (place) => {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  let navItems = document.getElementsByClassName("nav-item")
+  let navItems = document.querySelectorAll(".nav-item")
+  let activeNav = null
   window.addEventListener("scroll", function () {
     let currentPosition = window.scrollY
     for (const nav of navItems) {
       let navi = nav.querySelector(".nav-link")
       let section = document.getElementById(navi.innerHTML.toLowerCase())
-      if (section.offsetTop <= currentPosition && section.offsetTop + section.offsetHeight > currentPosition) {
-        navi.classList.add("active")
-      } else {
-        navi.classList.remove("active")
+      if (
+        section.offsetTop <= currentPosition &&
+        section.offsetTop + section.offsetHeight > currentPosition + 300
+      ) {
+        if (navi !== activeNav) {
+          if (activeNav) {
+            activeNav.classList.remove("active")
+          }
+          activeNav = navi
+          activeNav.classList.add("active")
+        }
+        break
       }
-
     }
   })
 })
